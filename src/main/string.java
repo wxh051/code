680e27d8c27b6b22e7c6656d01bad40feb9228fa
wxh <13546675295@163.com>
2022-03-26 16:15:05 +0800

    commit first

---
+++ src/main/string.java
@@ -0,0 +1,10 @@
+package main;
+
+import java.util.Scanner;
+
+public class string {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("hello");
+    }
+}
