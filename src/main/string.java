75571091a93c874ccbb2ac3c7f0068dd246c8edf
wxh <13546675295@163.com>
2022-03-26 16:17:25 +0800

    commit second

--- src/main/string.java
+++ src/main/string.java
@@ -6,5 +6,6 @@ public class string {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("hello");
+        System.out.println("hello 2");
     }
 }
